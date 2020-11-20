c8faa0e5427d985f8a17c3b1ef95b2dc2850f30a
bjartwolf <bjartnes@gmail.com>
2020-11-20 11:06:51 +0100

    init

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+type LanguageCode = LanguageCode of string
+
+type Subtitles = Language of LanguageCode
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (LanguageCode "norsk")
+    0 // return an integer exit code
